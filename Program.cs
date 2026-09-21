e0d486521cc9448253f2ca3670c92fcee0f33601
monika.hossain <monikahossain25@gmail.com>
2026-09-21 12:47:51 +0600

    feat(routing): update default controller route to Workspace

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Workspace}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
