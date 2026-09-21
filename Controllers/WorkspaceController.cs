9773120a2d9c8d1ce3bf7a7a064703834c66caf7
monika.hossain <monikahossain25@gmail.com>
2026-09-21 12:15:41 +0600

    add WorkspaceController with Index action

---
+++ Controllers/WorkspaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DockWorkspace.Controllers
+{
+    public class WorkspaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
