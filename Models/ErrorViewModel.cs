f1baa9ccbd8f3711575570beda87946b68268c8c
monika.hossain <monikahossain25@gmail.com>
2026-09-21 12:02:14 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DockWorkspace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
